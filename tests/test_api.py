46d50b9f0c03edb9da44eefe8df92a46b1e38568
Mishkin Evgeny <mjos1995@gmail.com>
2023-03-07 10:42:47 +0400

    Добавлен контейнер с тестами

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,10 +1,15 @@
-from datetime import datetime
-import requests
+import os
 import uuid
+from datetime import datetime
+from time import sleep
+
+import requests
+
+HOST, PORT = os.getenv('HOST'), os.getenv('PORT')
 
 
 def assert_balance(user, expected_balance, date=None):
-    url = f'http://localhost:8000/v1/user/{user["id"]}'
+    url = f'http://{HOST}:{PORT}/v1/user/{user["id"]}'
     if date:
         url += f'?date={date}'
     balance_resp = requests.get(url)
@@ -12,9 +17,8 @@ def assert_balance(user, expected_balance, date=None):
     assert balance_resp.json()['balance'] == expected_balance
 
 
-
 def test_api():
-    user_resp = requests.post('http://localhost:8000/v1/user', json={
+    user_resp = requests.post(f'http://{HOST}:{PORT}/v1/user', json={
         'name': 'petya'
     })
 
@@ -32,11 +36,11 @@ def test_api():
         'amount': '100.0',
         'timestamp': datetime(2023, 1, 4).isoformat(),  # technical field to make tests possible
     }
-    txn_resp = requests.post('http://localhost:8000/v1/transaction', json=txn)
+    txn_resp = requests.post(f'http://{HOST}:{PORT}/v1/transaction', json=txn)
     assert txn_resp.status_code == 200
     assert_balance(user, '100.00')
 
-    detail_resp = requests.get(f'http://localhost:8000/v1/transaction/{txn["uid"]}')
+    detail_resp = requests.get(f'http://{HOST}:{PORT}/v1/transaction/{txn["uid"]}')
     assert detail_resp.json()['type'] == 'DEPOSIT'
     assert detail_resp.json()['amount'] == '100.00'
 
@@ -47,9 +51,13 @@ def test_api():
         'amount': '50.0',
         'timestamp': datetime(2023, 1, 5).isoformat(),  # technical field to make tests possible
     }
-    txn_resp = requests.post('http://localhost:8000/v1/transaction', json=txn)
-    txn_resp = requests.post('http://localhost:8000/v1/transaction', json=txn)
+    
+    # Второй запрос с таким же UUID не должен выдвать статус 200, так как
+    # должен вызывать ошибку. Поэтому проверяем правильность исполнения только
+    # первого запроса
+    txn_resp = requests.post(f'http://{HOST}:{PORT}/v1/transaction', json=txn)
     assert txn_resp.status_code == 200
+    txn_resp = requests.post(f'http://{HOST}:{PORT}/v1/transaction', json=txn)
     assert_balance(user, '50.00')
 
 
@@ -60,7 +68,7 @@ def test_api():
         'amount': '60.0',
         'timestamp': datetime.utcnow().isoformat(),  # technical field to make tests possible
     }
-    txn_resp = requests.post('http://localhost:8000/v1/withdraw', json=txn)
+    txn_resp = requests.post(f'http://{HOST}:{PORT}/v1/transaction', json=txn)  # Ошибка? У нас нет точки v1/withdraw
     assert txn_resp.status_code == 402  # insufficient funds
     assert_balance(user, '50.00')
 
@@ -71,8 +79,15 @@ def test_api():
         'amount': '10.0',
         'timestamp': datetime(2023, 2, 5).isoformat(),  # technical field to make tests possible
     }
-    txn_resp = requests.post('http://localhost:8000/v1/transaction', json=txn)
+    txn_resp = requests.post(f'http://{HOST}:{PORT}/v1/transaction', json=txn)
     assert txn_resp.status_code == 200
     assert_balance(user, '40.00')
 
     assert_balance(user, '50.00', date='2023-01-30T00:00:00.00000000')
+
+
+if __name__ == "__main__":
+    sleep(5)  # Легкое решение, чтобы быть уверенным, что контейнер payment_service запустился
+    print('Running tests...')
+    test_api()
+    print('All tests has been passed')
